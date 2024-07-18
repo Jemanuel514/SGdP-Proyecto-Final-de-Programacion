bff31838ab16f982282d92ba51235578f068ca01
José Huertas <jemanuel0123@hotmail.com>
2024-07-18 14:18:04 -0500

    Correccion de Presentacion
    - Cambio de nombre de variables

--- SGdP_Proyecto_Final_de_Programacion/src/frontend/Presentacion.java
+++ SGdP_Proyecto_Final_de_Programacion/src/frontend/Presentacion.java
@@ -1,33 +1,28 @@
 package frontend;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import java.awt.Dimension;
-import java.awt.EventQueue;
-
-import javax.swing.JLabel;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
 
 import java.awt.Color;
+import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import javax.swing.SwingConstants;
-import javax.swing.JButton;
 
 public class Presentacion extends JFrame {
 
-	
 	private static final long serialVersionUID = 1L;
-	private JPanel contenedorPresentacion;
 
-
-  //Inicialización de la ventana
   	public static void main(String[] args) {
   		EventQueue.invokeLater(new Runnable() {
   			public void run() {
@@ -41,32 +36,21 @@ public class Presentacion extends JFrame {
   		});
   	}
   	
-	/**
-	 * Create the frame.
-	 */
 	public Presentacion() {
 		Path rutaProyecto = Paths.get(".").toAbsolutePath().normalize();
-		Path rutaDB = rutaProyecto.resolve("db/SGdP_DB.db");
 		
-		
-		setSize(new Dimension(1024, 768));
-		setResizable(false);
-		setMinimumSize(new Dimension(1024, 768));
-		setMaximumSize(new Dimension(1024, 768));
-		
-		setPreferredSize(new Dimension(1024, 768));
-		setTitle("Presentaci\u00F3n");
+		setSize(ConstantesEstilo.ventana);
+		setPreferredSize(ConstantesEstilo.ventana);
+		setResizable(false);		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
 		
-		contenedorPresentacion = new JPanel();
+		JPanel contenedorPresentacion = new JPanel();
 		contenedorPresentacion.setBackground(new Color(240, 255, 255));
 		contenedorPresentacion.setForeground(new Color(135, 206, 235));
 		contenedorPresentacion.setBorder(new EmptyBorder(5, 5, 5, 5));
-
-		setContentPane(contenedorPresentacion);
 		contenedorPresentacion.setLayout(null);
-		
+		setContentPane(contenedorPresentacion);
+
 		//LOGO UTP
 		//Cargar imagen
 		Path rutaLogoUTP = rutaProyecto.resolve("Resources/logoUTP.png");
@@ -100,91 +84,91 @@ public class Presentacion extends JFrame {
 		logoFISC.setIcon(iconoFISCredimensionado);
 		contenedorPresentacion.add(logoFISC);
 		
-		JLabel lblNewLabel_2 = new JLabel("UNIVERSIDAD TECNOL\u00D3GICA DE PANAM\u00C1");
-		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_2.setFont(new Font("Arial Unicode MS", Font.BOLD, 24));
-		lblNewLabel_2.setBounds(165, 11, 682, 58);
-		contenedorPresentacion.add(lblNewLabel_2);
+		JLabel lbl_universidad = new JLabel("UNIVERSIDAD TECNOLÓGICA DE PANAMÁ");
+		lbl_universidad.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_universidad.setFont(new Font("Arial Unicode MS", Font.BOLD, 24));
+		lbl_universidad.setBounds(165, 11, 682, 58);
+		contenedorPresentacion.add(lbl_universidad);
 		
-		JLabel lblNewLabel_2_1 = new JLabel("FACULTAD DE ING. DE SISTEMAS COMPUTACIONALES");
-		lblNewLabel_2_1.setHorizontalTextPosition(SwingConstants.CENTER);
-		lblNewLabel_2_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_2_1.setFont(new Font("Arial Unicode MS", Font.BOLD, 24));
-		lblNewLabel_2_1.setBounds(154, 67, 704, 58);
-		contenedorPresentacion.add(lblNewLabel_2_1);
+		JLabel lbl_facultad = new JLabel("FACULTAD DE ING. DE SISTEMAS COMPUTACIONALES");
+		lbl_facultad.setHorizontalTextPosition(SwingConstants.CENTER);
+		lbl_facultad.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_facultad.setFont(new Font("Arial Unicode MS", Font.BOLD, 24));
+		lbl_facultad.setBounds(154, 67, 704, 58);
+		contenedorPresentacion.add(lbl_facultad);
 		
-		JLabel lblNewLabel_2_1_1 = new JLabel("LICENCIATURA EN INGENIER\u00CDA DE SOFTWARE ");
-		lblNewLabel_2_1_1.setHorizontalTextPosition(SwingConstants.CENTER);
-		lblNewLabel_2_1_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_2_1_1.setFont(new Font("Arial Unicode MS", Font.BOLD, 24));
-		lblNewLabel_2_1_1.setBounds(180, 123, 651, 58);
-		contenedorPresentacion.add(lblNewLabel_2_1_1);
+		JLabel lbl_carrera = new JLabel("LICENCIATURA EN INGENIERÍA DE SOFTWARE ");
+		lbl_carrera.setHorizontalTextPosition(SwingConstants.CENTER);
+		lbl_carrera.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_carrera.setFont(new Font("Arial Unicode MS", Font.BOLD, 24));
+		lbl_carrera.setBounds(180, 123, 651, 58);
+		contenedorPresentacion.add(lbl_carrera);
 		
-		JLabel lblNewLabel_2_1_1_1 = new JLabel("GRUPO 1SF122");
-		lblNewLabel_2_1_1_1.setHorizontalTextPosition(SwingConstants.CENTER);
-		lblNewLabel_2_1_1_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_2_1_1_1.setFont(new Font("Artifakt Element", Font.BOLD, 24));
-		lblNewLabel_2_1_1_1.setBounds(375, 179, 262, 58);
-		contenedorPresentacion.add(lblNewLabel_2_1_1_1);
+		JLabel lbl_grupo = new JLabel("GRUPO 1SF122");
+		lbl_grupo.setHorizontalTextPosition(SwingConstants.CENTER);
+		lbl_grupo.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_grupo.setFont(new Font("Artifakt Element", Font.BOLD, 24));
+		lbl_grupo.setBounds(375, 179, 262, 58);
+		contenedorPresentacion.add(lbl_grupo);
 		
-		JLabel lblNewLabel_2_1_1_1_1 = new JLabel("FACILITADOR: RODRIGO Y\u00C1NG\u00DCEZ");
-		lblNewLabel_2_1_1_1_1.setHorizontalTextPosition(SwingConstants.CENTER);
-		lblNewLabel_2_1_1_1_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_2_1_1_1_1.setFont(new Font("Artifakt Element", Font.BOLD, 24));
-		lblNewLabel_2_1_1_1_1.setBounds(95, 571, 821, 58);
-		contenedorPresentacion.add(lblNewLabel_2_1_1_1_1);
+		JLabel lbl_profesor = new JLabel("FACILITADOR: RODRIGO YÁNGÜEZ");
+		lbl_profesor.setHorizontalTextPosition(SwingConstants.CENTER);
+		lbl_profesor.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_profesor.setFont(new Font("Artifakt Element", Font.BOLD, 24));
+		lbl_profesor.setBounds(95, 571, 821, 58);
+		contenedorPresentacion.add(lbl_profesor);
 		
-		JLabel lblNewLabel_2_1_1_1_2 = new JLabel("MIEMBROS DE EQUIPO:");
-		lblNewLabel_2_1_1_1_2.setHorizontalTextPosition(SwingConstants.CENTER);
-		lblNewLabel_2_1_1_1_2.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_2_1_1_1_2.setFont(new Font("Artifakt Element", Font.BOLD, 24));
-		lblNewLabel_2_1_1_1_2.setBounds(95, 235, 821, 58);
-		contenedorPresentacion.add(lblNewLabel_2_1_1_1_2);
+		JLabel lbl_miembros = new JLabel("MIEMBROS DE EQUIPO:");
+		lbl_miembros.setHorizontalTextPosition(SwingConstants.CENTER);
+		lbl_miembros.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_miembros.setFont(new Font("Artifakt Element", Font.BOLD, 24));
+		lbl_miembros.setBounds(95, 235, 821, 58);
+		contenedorPresentacion.add(lbl_miembros);
 		
-		JLabel lblNewLabel_2_1_1_1_2_1 = new JLabel("DANNA DAWKINS 8-1010-1542");
-		lblNewLabel_2_1_1_1_2_1.setHorizontalTextPosition(SwingConstants.CENTER);
-		lblNewLabel_2_1_1_1_2_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_2_1_1_1_2_1.setFont(new Font("Artifakt Element", Font.BOLD, 24));
-		lblNewLabel_2_1_1_1_2_1.setBounds(95, 291, 821, 58);
-		contenedorPresentacion.add(lblNewLabel_2_1_1_1_2_1);
+		JLabel lbl_danna = new JLabel("DANNA DAWKINS 8-1010-1542");
+		lbl_danna.setHorizontalTextPosition(SwingConstants.CENTER);
+		lbl_danna.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_danna.setFont(new Font("Artifakt Element", Font.BOLD, 24));
+		lbl_danna.setBounds(95, 291, 821, 58);
+		contenedorPresentacion.add(lbl_danna);
 		
-		JLabel lblNewLabel_2_1_1_1_2_1_1 = new JLabel("MANUEL GUILL\u00C9N 8-1016-1618");
-		lblNewLabel_2_1_1_1_2_1_1.setHorizontalTextPosition(SwingConstants.CENTER);
-		lblNewLabel_2_1_1_1_2_1_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_2_1_1_1_2_1_1.setFont(new Font("Artifakt Element", Font.BOLD, 24));
-		lblNewLabel_2_1_1_1_2_1_1.setBounds(95, 347, 821, 58);
-		contenedorPresentacion.add(lblNewLabel_2_1_1_1_2_1_1);
+		JLabel lbl_manuel = new JLabel("MANUEL GUILLÉN 8-1016-1618");
+		lbl_manuel.setHorizontalTextPosition(SwingConstants.CENTER);
+		lbl_manuel.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_manuel.setFont(new Font("Artifakt Element", Font.BOLD, 24));
+		lbl_manuel.setBounds(95, 347, 821, 58);
+		contenedorPresentacion.add(lbl_manuel);
 		
-		JLabel lblNewLabel_2_1_1_1_2_1_2 = new JLabel("JOS\u00C9 HUERTAS 2-754-1176");
-		lblNewLabel_2_1_1_1_2_1_2.setHorizontalTextPosition(SwingConstants.CENTER);
-		lblNewLabel_2_1_1_1_2_1_2.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_2_1_1_1_2_1_2.setFont(new Font("Artifakt Element", Font.BOLD, 24));
-		lblNewLabel_2_1_1_1_2_1_2.setBounds(95, 403, 821, 58);
-		contenedorPresentacion.add(lblNewLabel_2_1_1_1_2_1_2);
+		JLabel lbl_jose = new JLabel("JOSÉ HUERTAS 2-754-1176");
+		lbl_jose.setHorizontalTextPosition(SwingConstants.CENTER);
+		lbl_jose.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_jose.setFont(new Font("Artifakt Element", Font.BOLD, 24));
+		lbl_jose.setBounds(95, 403, 821, 58);
+		contenedorPresentacion.add(lbl_jose);
 		
-		JLabel lblNewLabel_2_1_1_1_2_1_2_1 = new JLabel("JOAQU\u00CDN LU 8-1024-2466");
-		lblNewLabel_2_1_1_1_2_1_2_1.setHorizontalTextPosition(SwingConstants.CENTER);
-		lblNewLabel_2_1_1_1_2_1_2_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_2_1_1_1_2_1_2_1.setFont(new Font("Artifakt Element", Font.BOLD, 24));
-		lblNewLabel_2_1_1_1_2_1_2_1.setBounds(95, 459, 821, 58);
-		contenedorPresentacion.add(lblNewLabel_2_1_1_1_2_1_2_1);
+		JLabel lbl_joaquin = new JLabel("JOAQUÍN LU 8-1024-2466");
+		lbl_joaquin.setHorizontalTextPosition(SwingConstants.CENTER);
+		lbl_joaquin.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_joaquin.setFont(new Font("Artifakt Element", Font.BOLD, 24));
+		lbl_joaquin.setBounds(95, 459, 821, 58);
+		contenedorPresentacion.add(lbl_joaquin);
 		
-		JLabel lblNewLabel_2_1_1_1_2_1_2_2 = new JLabel("ANIE LUO 8-1016-414");
-		lblNewLabel_2_1_1_1_2_1_2_2.setHorizontalTextPosition(SwingConstants.CENTER);
-		lblNewLabel_2_1_1_1_2_1_2_2.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_2_1_1_1_2_1_2_2.setFont(new Font("Artifakt Element", Font.BOLD, 24));
-		lblNewLabel_2_1_1_1_2_1_2_2.setBounds(95, 515, 821, 58);
-		contenedorPresentacion.add(lblNewLabel_2_1_1_1_2_1_2_2);
+		JLabel lbl_anie = new JLabel("ANIE LUO 8-1016-414");
+		lbl_anie.setHorizontalTextPosition(SwingConstants.CENTER);
+		lbl_anie.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_anie.setFont(new Font("Artifakt Element", Font.BOLD, 24));
+		lbl_anie.setBounds(95, 515, 821, 58);
+		contenedorPresentacion.add(lbl_anie);
 		
-		JLabel lblNewLabel_2_1_1_1_2_1_2_3 = new JLabel("FECHA DE ENTREGA: 19 DE JULIO DE 2024");
-		lblNewLabel_2_1_1_1_2_1_2_3.setHorizontalTextPosition(SwingConstants.CENTER);
-		lblNewLabel_2_1_1_1_2_1_2_3.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_2_1_1_1_2_1_2_3.setFont(new Font("Artifakt Element", Font.BOLD, 24));
-		lblNewLabel_2_1_1_1_2_1_2_3.setBounds(95, 627, 821, 58);
-		contenedorPresentacion.add(lblNewLabel_2_1_1_1_2_1_2_3);
+		JLabel lbl_fecha = new JLabel("FECHA DE ENTREGA: 19 DE JULIO DE 2024");
+		lbl_fecha.setHorizontalTextPosition(SwingConstants.CENTER);
+		lbl_fecha.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_fecha.setFont(new Font("Artifakt Element", Font.BOLD, 24));
+		lbl_fecha.setBounds(95, 627, 821, 58);
+		contenedorPresentacion.add(lbl_fecha);
 		
-		JButton botonEmpezar = new JButton("Empecemos");
-		botonEmpezar.addActionListener(new ActionListener() {
+		JButton boton_empezar = new JButton("Empecemos");
+		boton_empezar.addActionListener(new ActionListener() {
   			public void actionPerformed(ActionEvent e) {
   				dispose();
 				InicioSesion iniciarSesion = new InicioSesion();
@@ -192,11 +176,11 @@ public class Presentacion extends JFrame {
   			}
   		});
 		
-		botonEmpezar.setForeground(new Color(0, 0, 0));
-		botonEmpezar.setBackground(new Color(173, 216, 230));
-		botonEmpezar.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 20));
-		botonEmpezar.setBounds(818, 640, 162, 33);
-		contenedorPresentacion.add(botonEmpezar);
+		boton_empezar.setForeground(new Color(0, 0, 0));
+		boton_empezar.setBackground(new Color(173, 216, 230));
+		boton_empezar.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 20));
+		boton_empezar.setBounds(818, 640, 162, 33);
+		contenedorPresentacion.add(boton_empezar);
 		
 		//IMAGEN DE GATO NYEHEHE
 		//Cargar imagen
